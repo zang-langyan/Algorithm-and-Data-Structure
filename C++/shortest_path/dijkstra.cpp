63b2956acffce56d6fbbc6ba988c0a03c3dfae85
Langyan <langyan_zang@163.com>
2024-11-06 11:59:34 +0800

    Update 2024-11-6 12:00 Add Dijkstra Algorithm using priority_queue (C++)

--- C++/shortest_path/dijkstra.cpp
+++ C++/shortest_path/dijkstra.cpp
@@ -1,8 +1,64 @@
+#include <functional>
 #include <iostream>
+#include <list>
+#include <queue>
+#include <utility>
 #include <vector>
 #include <climits>
 
 using namespace std;
+
+ostream& operator<<(ostream& os, const vector<int>& v){
+  for (auto& i: v){
+    os << i << " "; 
+  }
+  os << endl;
+  return os;
+}
+
+// priority_queue version
+class Dijkstra 
+{
+private:
+  int m_numNodes;
+  vector<vector<int>> m_edges;
+  vector<vector<pair<int,int>>> m_graph;
+  vector<vector<int>> shortest_path;
+public:
+  explicit Dijkstra(int n_nodes, const vector<vector<int>>& edges) 
+    : m_numNodes(n_nodes), m_edges(edges) 
+  {
+    m_graph.resize(m_numNodes);
+    shortest_path.resize(m_numNodes);
+    for (const auto& e : m_edges){
+      int vert_start = e[0], vert_end = e[1], dist = e[2];
+      m_graph[vert_start].emplace_back(vert_end, dist);
+    }
+  };
+
+  vector<int> solve(int root){
+    if (!shortest_path[root].empty()) return shortest_path[root];
+    vector<int> ans(m_numNodes, INT_MAX);
+    ans[root] = 0;
+
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+    pq.emplace(0, root);
+    while (!pq.empty()){
+      auto [d, cur] = pq.top();
+      pq.pop();
+      if (d > ans[cur]) continue;
+      for (const auto& [nxt, d2nxt]: m_graph[cur]){
+        if (ans[nxt] > ans[cur] + d2nxt){
+          ans[nxt] = ans[cur] + d2nxt;
+          pq.emplace(ans[nxt], nxt);
+        }
+      }
+    }
+    shortest_path[root] = ans;
+    return ans;
+  }
+
+};
 
 vector<int> dijkstra(int root, const vector<vector<int>>& g){
   int n = g.size();
@@ -39,13 +95,6 @@ vector<int> dijkstra(int root, const vector<vector<int>>& g){
   return l;
 }
 
-ostream& operator<<(ostream& os, const vector<int>& v){
-  for (auto& i: v){
-    os << i << " "; 
-  }
-  os << endl;
-  return os;
-}
 
 int main(){
   int n = 6;
@@ -61,6 +110,11 @@ int main(){
     {4, 3, 5},
     {5, 4, 3}
   };
+
+  Dijkstra shortest_path_solver = Dijkstra(n, edges);
+  cout << shortest_path_solver.solve(0);
+
+
   vector<vector<int>> g(n, vector<int>(n));
 
   for (auto& e: edges){
